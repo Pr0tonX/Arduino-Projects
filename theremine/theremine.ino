e1f3e8d462fef19ad8baa21698956264223af791
Raphaël Perraud <Hav4na@protonmail.com>
2019-04-06 01:06:38 +0200

    Add files via upload

--- theremine/theremine.ino
+++ theremine/theremine.ino
@@ -23,18 +23,21 @@
 */
 
 //#include <ADC.h>  // Teensy 3.0/3.1 uncomment this line and install http://github.com/pedvide/ADC
+//#include <util/atomic.h>
 #include <MozziGuts.h>
 #include <Oscil.h> // oscillator template
 #include <tables/sin2048_int8.h> // sine table for oscillator
+#include <tables/cos8192_int8.h>
 #include <tables/triangle_valve_2048_int8.h>
 #include <RollingAverage.h>
 #include <ControlDelay.h>
 #include <mozzi_midi.h>
 #include <mozzi_fixmath.h>
+//#include <util/atomic.h>
 
-#define INPUT_PIN 0 // analog control input: tone
-const char VOL_PIN = 2 ;//analog control input : volume
-
+#define note_PIN 0 // analog control input: tone
+const char pulse_PIN = 2 ;//analog control input : volume
+char v0;
 
 unsigned int echo_cells_1 = 32;
 unsigned int echo_cells_2 = 60;
@@ -46,6 +49,8 @@ Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin1(SIN2048_DATA);
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin2(SIN2048_DATA);
 Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> aSin3(SIN2048_DATA);
 
+Oscil<COS8192_NUM_CELLS, CONTROL_RATE> kVol0(COS8192_DATA);
+Oscil<COS8192_NUM_CELLS, AUDIO_RATE> aCos0(COS8192_DATA);
 int pValue ;
 byte volume;
 
@@ -57,31 +62,60 @@ RollingAverage <int, 32> kAverage; // how_many_to_average has to be power of 2
 int averaged;
 
 
+#define LDR_PIN 2
+#define NUM_VOICES 8
+#define THRESHOLD 10
+// notes to play depending on whether note reading increases or decreases
+float upnotes[NUM_VOICES] = {
+  mtof(62.f),mtof(64.f),mtof(70.f),mtof(72.f),mtof(79.f),mtof(81.f), mtof(86.f), mtof(88.f)};
+
+float downnotes[NUM_VOICES] = {
+  mtof(64.f),mtof(65.f),mtof(88.f),mtof(72.f),mtof(79.f),mtof(84.f),mtof(86.f),mtof(89.f)};
+  
+
+
 void setup() {
  // Serial.begin(115200);// set up the Serial output so we can look at the piezo values // set up the Serial output so we can look at the analog levels
   kDelay.set(echo_cells_1);
-
+ // Serial.begin(9600);
   startMozzi(CONTROL_RATE);
 }
+  
+// returns freq
+int noteToFreq(char oscil_num, int note){
+  static int previous_note;
+  int freq;
+  if (note>previous_note){
+    freq = upnotes[oscil_num];
+  } else {
+     freq = downnotes[oscil_num];
+  }
+  previous_note = note;
+  return freq;
+}
+
 
 void updateControl() {
-  ////////////////// VOLUME ///////////////////////////////
-  pValue = mozziAnalogRead(VOL_PIN); // value is 0-600 ~
- //Serial.print("value : ");
- //Serial.println( pValue);
+   static float previous_pulse_freq;   
 
-  // map it to an 8 bit volume range for efficient calculations in updateAudio
-  //volume = map (pValue, 0, 1023, 255, 0);  // 10 bits (0->1023) shifted right by 2 bits to give 8 bits (0->255) 
-  volume =255 -(pValue >>2);
-/*  if (volume > 100) volume = 100;
-  if (volume < 0)volume = 0; */
-//  Serial.print("volume = ");
-  //Serial.println(volume);
+  ////////////////// PULSE ///////////////////////////////
+  int pulse = mozziAnalogRead(pulse_PIN); // value is 0-600 ~
+    int note = mozziAnalogRead(note_PIN); 
+
+
+
+  // map pulse reading to volume pulse frequency
+  float pulse_freq = (float)pulse/256;
+  previous_pulse_freq = pulse_freq;
+  kVol0.setFreq(pulse_freq);
+  if(abs(v0)<THRESHOLD) aCos0.setFreq(noteToFreq(0,note));
+
+  v0 = kVol0.next();
+  //Serial.println(v0);
   
   ////////////////////////// TONE //////////////////////
 
-  int bumpy_input = mozziAnalogRead(INPUT_PIN);
-  averaged = kAverage.next(bumpy_input);
+  averaged = kAverage.next(note);
   aSin0.setFreq(averaged);
   aSin1.setFreq(kDelay.next(averaged));
   aSin2.setFreq(kDelay.read(echo_cells_2));
@@ -89,12 +123,8 @@ void updateControl() {
 }
 
 int updateAudio() {
-  return ((int)((
-    (((int)aSin0.next()) + 
-    ((aSin1.next()) + 
-    (((aSin2.next() >> 1))) + 
-    (((aSin3.next() >> 2))) 
-    )>> 3) *volume ) >>8)) ;
+ return v0 / 16 * ((int)aSin0.next()+aSin1.next()+(aSin2.next()>>1)
+    +(aSin3.next()>>2)) >> 6;
 }
 
 
